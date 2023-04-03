cd68bd4bcc2d0fb4b1bc1fefbfc8db2d765bd3f9
Marius CA <mariuskcodeacademy@gmail.com>
2023-04-03 10:35:19 +0300

    prop types

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import './styles/reset.css';
 import './styles/App.css';
 import Grid from './components/ui/grid/Grid';
+import Card from './components/ui/card/Card';
 
 function App() {
   return (
     <div className="container">
-      <Grid cols={'2'}>
+      <Grid cols={1}>
         <h1>New project</h1>
+        <p>HEllo</p>
+        {/* James Bond */}
       </Grid>
+      <Card className={'tac'}>
+        <h2>New project</h2>
+        <p>HEllo</p>
+      </Card>
     </div>
   );
 }
--- src/components/ui/card/Card.jsx
+++ src/components/ui/card/Card.jsx
@@ -1,6 +1,12 @@
+import PropTypes from 'prop-types';
+
 import './card.css';
 
-function Card({ children }) {
-  return <div className='card'>{children}</div>;
+function Card({ children, className }) {
+  return <div className={`card ${className}`}>{children}</div>;
 }
+Card.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.array.isRequired,
+};
 export default Card;
